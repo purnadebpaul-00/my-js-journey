ce149bae1d35f9aa7d03c443ebbf3ec523f07514
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-09-29 07:45:34 +0000

    conversionOperation done

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,73 @@
+/* =====number to string=======
+
+let number = 33;
+console.log(typeof number);
+
+let numbertostring = String(number);
+console.log(numbertostring);
+console.log(typeof numbertostring);
+
+*/
+
+
+/* ======= string to number =========
+
+let string = "sanju";
+console.log(string);
+console.log(typeof string);
+
+let stringtonumber = Number(string);
+console.log(typeof stringtonumber);
+console.log(stringtonumber);
+
+*/
+
+/* =========number to boolean==========
+
+let number = 70;
+console.log(number);
+console.log(typeof number);
+
+let numbertoboolean = Boolean(number);
+console.log(typeof numbertoboolean);
+console.log(numbertoboolean);
+
+*/
+
+
+/* ======= boolean to number ======
+let boolean = false;
+console.log(boolean);
+console.log(typeof boolean);
+
+let booleantonumber = Number(boolean);
+console.log(typeof booleantonumber);
+console.log(booleantonumber);
+
+*/
+
+/*========== string to boolean ========
+let string = "";
+console.log(string);
+console.log(typeof string);
+
+let stringtoboolean = Boolean(string);
+console.log(typeof stringtoboolean);
+console.log(stringtoboolean);
+
+//empty string will return false
+
+*/
+
+/* =========boolean to string ========= 
+
+let boolean = true;
+console.log(typeof boolean);
+console.log(boolean);
+
+let booleantostring = String(boolean);
+console.log(typeof booleantostring);
+console.log(booleantostring);
+
+*/
+
