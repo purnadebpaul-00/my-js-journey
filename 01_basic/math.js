0ad160a9b3c8fc47461cd2d0db67456ce92a0612
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-10-03 10:18:17 +0000

    math done

---
+++ 01_basic/math.js
@@ -0,0 +1,53 @@
+
+// Math.round
+console.log(Math.round(5.4));   //rounded to its nearest integer
+console.log(Math.round(5.5));
+console.log(Math.round(5.6));
+
+console.log("\n======\n")
+
+// Math.ceil
+console.log(Math.ceil(5.2));   //rounded UP to its nearest integer
+console.log(Math.ceil(5.8));
+
+console.log("\n======\n")
+
+// Math.floor
+console.log(Math.floor(5.6));   //Returns x rounded down to its nearest integer
+console.log(Math.floor(5.1));
+
+console.log("\n======\n")
+
+// Math.trunc
+console.log(Math.trunc(52.11544));  //Returns the integer part
+
+console.log("\n======\n")
+
+// Math.sign
+console.log(Math.sign(0));  //returns if number is negative, null or positive
+
+console.log("\n======\n")
+
+// Math.pow
+console.log(Math.pow(2,3));  // returns the value of x to the power of y
+console.log(Math.pow(5,2));
+
+console.log("\n======\n")
+
+// Math.sqrt
+console.log(Math.sqrt(64));
+console.log(Math.sqrt(25));
+
+console.log("\n======\n")
+
+// Math.min() and Math.max()
+console.log(Math.min(10, 20, 0, 500, -10, 50));  // used to find the lowest or highest value in a list of arguments
+console.log(Math.max(10, 20, 0, 500, -10, 50));
+
+console.log("\n======\n")
+
+// Math.log
+console.log(Math.log(10));  //returns the natural logarithm of any number
+console.log(Math.log(100));
+
+
