1cc26f0dfafbff558808e536c24a38d402351867
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-10-03 10:41:52 +0000

    first game

---
+++ 01_basic/dice-roller-math.js
@@ -0,0 +1,17 @@
+/*// Math.random (most important topic)
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+console.log(Math.floor(Math.random()*10) + 1);
+
+*/
+// Dice game
+
+let min = 1;
+let max = 6;
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min));
+
+
+
