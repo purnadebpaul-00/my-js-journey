ccdabeac32df7b987d31e3f3e31077f5ca411132
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-10-11 20:52:52 +0600

    Update object.js
    de structure

--- 01_basic/object.js
+++ 01_basic/object.js
@@ -47,8 +47,22 @@ const User = {
         permalocation : "Dhanmondi"
     }
 }
-console.log(User);
-console.log(User.nameinfo);
-console.log(User.nameinfo.usernameinfo);
-console.log(User.nameinfo.usernameinfo.username);
-console.log(User.locationinfo.permalocation);
+// console.log(User);
+// console.log(User.nameinfo);
+// console.log(User.nameinfo.usernameinfo);
+// console.log(User.nameinfo.usernameinfo.username);
+// console.log(User.locationinfo.permalocation);
+
+console.log(`My name is ${User.nameinfo.firstname} ${User.nameinfo.lastname} I create a facebook account my user name is ${User.nameinfo.usernameinfo.username} my current location is ${User.locationinfo.currentlocation} and my permanent location is ${User.locationinfo.permalocation}`);
+
+// OBJECT DE STRUCTURE
+
+// const {firstname = fn} = nameinfo
+// const {lastname = ln} = nameinfo
+const {nameinfo: {firstname: FN}} = User
+const {nameinfo: {lastname: LN}} = User
+const {nameinfo: {usernameinfo:{username: UN}}} = User
+const {locationinfo:{currentlocation: CLOCA}} = User
+const {locationinfo:{permalocation: PLOCA}} = User
+console.log(`Hello my name is ${FN} ${LN} I create an instagram account my user name is ${UN} my current location is ${CLOCA} and permanent location is ${PLOCA}`);
+console.log(nameinfo);
