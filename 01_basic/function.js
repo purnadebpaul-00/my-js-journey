39f225a52d45937361e52566e8f76b224bf79687
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-10-28 17:38:36 +0000

    function

---
+++ 01_basic/function.js
@@ -0,0 +1,65 @@
+// =============== Function Declarations ======================
+
+/*
+
+function function_name(parameters){
+    // code to be executed
+}
+
+// === EXM ===
+function myFunction(a, b) {
+  return a * b;
+}
+
+console.log(myFunction(4,5))
+
+*/
+
+// ================= Function Expressions ======================
+
+/*
+const function_name = function(parameters){
+    // code to be executed
+}
+
+// === EXM ===
+
+const myFunction = function (a, b){
+    return a * b
+}
+
+console.log(myFunction(2,4))
+*/
+
+// ==================== Arrow Functions =========================
+
+/*
+const function_name = (parameters) => {
+    // code to be executed
+}
+
+// === EXM ===
+
+const myFunction = (a,b) => {
+    return a * b
+}
+console.log(myFunction(5,6));
+
+*/
+
+// ===================== implicit return ========================
+
+/*
+const myFunction = (a,b,c,d) => (a+b+c+d)
+console.log(myFunction(1,2,3,4));
+
+// +++++++ we need to write return if we use curly braces and we don't need to write return if we use normal brace ++++++++++++++
+
+*/
+
+
+
+
+
+
+
