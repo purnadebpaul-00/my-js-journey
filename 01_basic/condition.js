e650d328447f41e8a910b1309c5c4dc16b37cab4
purnadebpaul-00 <146330922+purnadebpaul-00@users.noreply.github.com>
2023-10-29 06:52:48 +0000

    some info about conditon

---
+++ 01_basic/condition.js
@@ -0,0 +1,15 @@
+const money = 1000
+
+
+if(money > 500 && money <=700){
+    console.log("kacchi");
+}
+else if(money > 700 && money <= 900){
+    console.log("pizza");
+}
+else if(money > 900 && money <=1000){
+    console.log("pizza, burger, kacchi");
+}
+else{
+    console.log("kichui naa");
+}
